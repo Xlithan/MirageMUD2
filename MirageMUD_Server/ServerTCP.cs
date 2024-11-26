80584e205a2b3b54aadc6403c1395d118054881d
Windows <XlithansLittleHelper@Work>
2024-11-26 22:14:52 +0000

    Fix order of logs

--- MirageMUD_Server/ServerTCP.cs
+++ MirageMUD_Server/ServerTCP.cs
@@ -20,22 +20,38 @@ namespace MirageMUD_Server
 
         private void OnClientConnect(IAsyncResult ar)
         {
-            TcpClient client = ServerSocket.EndAcceptTcpClient(ar);
-            client.NoDelay = false;
-            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
+            // Get the connecting client
+            var connectingTcpClient = ServerSocket.EndAcceptTcpClient(ar);
 
-            for(int i = 1; i <= Constants.MAX_PLAYERS; i++)
+            // Determine the IP of the incoming connection
+            var sourceIp = connectingTcpClient.Client.RemoteEndPoint?.ToString();
+
+            Console.WriteLine($"Connection received from {sourceIp}");
+
+            // Disable Nagle's algorithm
+            connectingTcpClient.NoDelay = false;
+            
+            // For every client slot
+            for (int i = 1; i <= Constants.MAX_PLAYERS; i++)
             {
+                // If the client slot has a null socket
                 if (Clients[i].Socket == null)
                 {
-                    Clients[i].Socket = client;
+                    // Assign the incoming connection to this slot
+                    Clients[i].Socket = connectingTcpClient;
                     Clients[i].Index = i;
-                    Clients[i].IP = client.Client.RemoteEndPoint.ToString();
+                    Clients[i].IP = sourceIp;
+
+                    // Start receiving data from the client
                     Clients[i].Start();
-                    Console.WriteLine("Connection received from " + Clients[i].IP + ".");
-                    return;
+
+                    // Break out of the for loop now that the client has been assigned successfully
+                    break;
                 }
             }
+
+            // Start waiting for the next connection to come in
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
         }
     }
 }
