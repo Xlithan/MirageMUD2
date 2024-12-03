05fa0a833e1a821944c1dc76c0e098bda2a9fc93
Xlithan <steve.jones487@gmail.com>
2024-12-03 12:39:29 +0000

    WPF Updates

--- MirageMUD2_WPFClient/MainWindow.xaml.cs
+++ MirageMUD2_WPFClient/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Navigation;
-using MirageMUD2_WPFClient.View.Pages;
-using MirageMUD2_WPFClient.View.UserControls;
 
 namespace MirageMUD2_WPFClient
 {
@@ -10,28 +7,6 @@ namespace MirageMUD2_WPFClient
         public MainWindow()
         {
             InitializeComponent();
-
-            // Hide the navigation UI (back/forward buttons)
-            MainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
-
-            // Load the Home Page into the MainFrame when the application starts
-            MainFrame.Navigate(new Page_Home());
-
-            // Subscribe to the Page Requested event from the instance of NavMenu
-            navMenu.HomePageRequested += NavigateToHomePage;
-            navMenu.NewAccPageRequested += NavigateToNewAccPage;
-        }
-
-        // Navigation handler
-        private void NavigateToHomePage(object sender, RoutedEventArgs e)
-        {
-            // Navigate to Page_Home
-            MainFrame.Navigate(new Page_Home());
-        }
-        private void NavigateToNewAccPage(object sender, RoutedEventArgs e)
-        {
-            // Navigate to Page_Home
-            MainFrame.Navigate(new Page_NewAcc());
         }
     }
 }
