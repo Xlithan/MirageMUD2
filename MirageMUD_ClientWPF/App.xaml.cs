b342711397a5a8450e940a96b4fd82e69058e66d
Xlithan <steve.jones487@gmail.com>
2024-12-11 18:23:00 +0000

    Added WPF Client project

---
+++ MirageMUD_ClientWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MirageMUD_ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
